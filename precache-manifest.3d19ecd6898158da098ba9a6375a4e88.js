d1fde0422ff8fa159389dd69c06a34437f625a77
Nexhan <subodhkhadka3@gmail.com>
2020-07-03 21:58:59 -0700

    Updates

---
+++ precache-manifest.3d19ecd6898158da098ba9a6375a4e88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eedf71f65e6e553646a5cdaf4355dcb",
+    "url": "/CoinFlicker/index.html"
+  },
+  {
+    "revision": "138020c00c9948331cff",
+    "url": "/CoinFlicker/static/css/main.dc9c582d.chunk.css"
+  },
+  {
+    "revision": "d3b25f761239f1d62591",
+    "url": "/CoinFlicker/static/js/2.d06e2bc1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CoinFlicker/static/js/2.d06e2bc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "138020c00c9948331cff",
+    "url": "/CoinFlicker/static/js/main.9272058f.chunk.js"
+  },
+  {
+    "revision": "c391244d7187b7895968",
+    "url": "/CoinFlicker/static/js/runtime-main.64e3b6ac.js"
+  }
+]);
