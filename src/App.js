a3a439bb04cad3682ee9e0b209a2f3862ada69e1
Nexhan <subodhkhadka3@gmail.com>
2020-07-03 21:58:27 -0700

    deploy my coin flicker app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import CoinFlicker from './CoinFlicker';
+import './App.css';
+
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <CoinFlicker />
+      </div>
+    );
+
+  }
+}
+
+export default App;
